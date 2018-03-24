7ddbc7a9bea907e4b4cf7d4fd7cecfe816c5c886
Anders Lassen <develop@lassena.dk>
2018-03-24 22:21:20 +0100

    testing connection

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Npgsql;
+
+class Sample
+{
+    static void Main(string[] args)
+    {
+        // Connect to PostgreSQL  database
+        NpgsqlConnection dbconn = new NpgsqlConnection("Server=127.0.0.1; User Id = postgres; " + " Password = postgres; Database = postgres");
+        dbconn.Open();
+
+        // Define a query returning a single row result set
+        NpgsqlCommand query = new NpgsqlCommand("SELECT * FROM information_schema.tables", dbconn);
+
+        // Execute the query and obtain a result set
+        NpgsqlDataReader dr = query.ExecuteReader();
+
+
+        while (dr.Read())
+            Console.Write("{0}\t{1}\t{2} \n", dr[0], dr[1], dr[2]);
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey(); //keep console window open in debug mode
+
+        dbconn.Close();
+    }
+}
+
